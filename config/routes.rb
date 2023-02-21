7fc30805a7b9c16004a3039c6496937ce79aabf4
Nnamdi Dibigbo <nnamdi.dibigbo@gmail.com>
2023-02-21 15:45:50 +0000

    Challenge 2 Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
+  get "tasks", to: "tasks#index"
+  get "task/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  get "task/:id", to: "tasks#show", as: :task
+
 end
